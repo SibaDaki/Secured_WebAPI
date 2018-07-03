142b5384030aa8e7d284743ca24c7b61b2e111e2
SibaDaki <dakicyb@gmail.com>
2018-07-03 16:27:20 +0200

    Add files via upload

---
+++ WebAPI/WebAPI/Controllers/TokenController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public object CustomClaimTypes { get; private set; }
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetResult()
+        {
+            var userName = User.Identity.Name;
+
+            return Ok($"Super secret content, I hope you've got clearance for this {userName}...");
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequest request)
+        {
+            if (request.Username == "Ande" && request.Password == "1215")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username),
+                    new Claim("CompletedBasicTraining","")
+
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "yourdomain.com",
+                    audience: "yourdomain.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+        
+    }
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
